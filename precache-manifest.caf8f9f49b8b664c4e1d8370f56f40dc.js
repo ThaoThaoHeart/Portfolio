63f5b88b906522d1fd7482af5c03e03d45a3bc0e
Jenny Hoang <jennyhoang_9@hotmail.com>
2021-11-30 16:58:49 -0500

    Updates

---
+++ precache-manifest.caf8f9f49b8b664c4e1d8370f56f40dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf522681ceea1927259a43c6e8adbd5a",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "ba6f1ea4aeb8f573285b",
+    "url": "/Portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "33b1a0eb0d257c14a0e1",
+    "url": "/Portfolio/static/css/main.db6f0745.chunk.css"
+  },
+  {
+    "revision": "ba6f1ea4aeb8f573285b",
+    "url": "/Portfolio/static/js/2.6ebf356d.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/Portfolio/static/js/2.6ebf356d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33b1a0eb0d257c14a0e1",
+    "url": "/Portfolio/static/js/main.37cd1d5d.chunk.js"
+  },
+  {
+    "revision": "c125469937b460e6de34",
+    "url": "/Portfolio/static/js/runtime-main.3379861c.js"
+  }
+]);
